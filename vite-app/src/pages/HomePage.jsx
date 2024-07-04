55a39ea6826cba0addcf37e12b63040fde6493b6
Sean Schulte <sirsean@gmail.com>
2024-07-04 21:47:29 +0000

    better home page text

--- vite-app/src/pages/HomePage.jsx
+++ vite-app/src/pages/HomePage.jsx
@@ -1,13 +1,11 @@
-import { Link } from 'react-router-dom';
-
 export default function HomePage() {
   return (
     <div className="HomePage">
-      <p>You are here because you like defi.</p>
-      <p>You are here because you like NFTs.</p>
-      <p>You are here because you like Dinero Protocol.</p>
-      <p>You are here because you like eyeballs.</p>
-      <p>Those things are all <em>weird</em>. But just how weird are you? This shit is pretty fucking weird.</p>
+      <p><a href="https://dineroismoney.com" target="_blank">Dinero Protocol</a> is pushing the boundaries of decentralized money, and we're here because we dig that. They're doing some cool stuff over there.</p>
+      <p>For some reason, their logo is an eyeball.</p>
+      <p>Eyeballer is an NFT project, in honor of Dinero but not affiliated with it, that consists of 10k <em>unique</em> images of eyeballs. Unlike yours, these ones don't come in pairs.</p>
+      <p>Show your Dinero appreciation, pluck one for yourself, be an Eyeballer.</p>
+      <p>* I hope you get a picture you like! You should not really expect it to increase in value, or to get any airdrops. We're doing this for fun.</p>
     </div>
   );
 }
